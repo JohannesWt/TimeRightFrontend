da87aa5b31f13012d70fdc6dd186b43b252d0792
julian <julian.boerste@gmx.de>
2019-10-22 11:50:48 +0200

    test

--- lib/ui/views/absence_choice_view.dart
+++ lib/ui/views/absence_choice_view.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class AbsenceChoiceView extends StatelessWidget {
+  AbsenceChoiceView({@required this.onPressed});
+
+  final GestureTapCallback onPressed;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Abwesenheit'),),
+        appBar: AppBar(title: Text('Abwesenheit')),
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            FlatButton(
+                onPressed: () {},
+               child: Row(
+                  children: <Widget>[
+                    Text("Urlaub beantragen ",textScaleFactor: 1.3),
+                    Expanded(
+
+                        child: Align(
+
+                            alignment: Alignment.centerRight,
+
+                            child: Icon(Icons.arrow_forward_ios)))
+                  ],
+                )
+
+            ),
+            FlatButton(
+                onPressed: () {},
+                child: Row(
+                  children: <Widget>[
+                    Text("Gleittag beantragen ",textScaleFactor: 1.3),
+                    Expanded(
+
+                        child: Align(
+
+                            alignment: Alignment.centerRight,
+
+                            child: Icon(Icons.arrow_forward_ios)))
+                  ],
+                )
+
+            ),
+            FlatButton(
+                onPressed: () {},
+                child: Row(
+                  children: <Widget>[
+                    Text("Krankmeldung ",textScaleFactor: 1.3),
+                    Expanded(
+
+                        child: Align(
+
+                            alignment: Alignment.centerRight,
+
+                            child: Icon(Icons.arrow_forward_ios)))
+                  ],
+                )
+
+            )
+
+
+
+          ],
+        )
     );
   }
+
+
 }
